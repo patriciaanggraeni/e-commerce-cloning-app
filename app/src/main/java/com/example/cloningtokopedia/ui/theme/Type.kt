8f94ac1c4eb5cd7b2db835468bcae52df7e3d85c
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-10-24 20:38:31 +0700

    settingup font

--- app/src/main/java/com/example/cloningtokopedia/ui/theme/Type.kt
+++ app/src/main/java/com/example/cloningtokopedia/ui/theme/Type.kt
@@ -2,17 +2,20 @@ package com.example.cloningtokopedia.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.cloningtokopedia.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+val openSauceOneLight = FontFamily(
+    Font(R.font.open_sauce_sans_light)
+)
+
+val openSauceOneRegular = FontFamily(
+    Font(R.font.open_sauce_one_regular)
+)
+
+val openSauceOneBold = FontFamily(
+    Font(R.font.open_sauce_one_bold)
 )
