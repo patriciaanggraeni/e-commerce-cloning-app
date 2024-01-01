67d92829d6b2a32ef567607501beeb254a45b181
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2024-01-01 19:39:11 +0700

    > renamed: CustomSearchField.kt -> Search.kt > renamed: CustomSearchField -> CustomSearch (composable function) > update details

--- app/src/main/java/com/example/cloningtokopedia/customization/Search.kt
+++ app/src/main/java/com/example/cloningtokopedia/customization/Search.kt
@@ -2,12 +2,10 @@ package com.example.cloningtokopedia.customization
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -15,7 +13,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,19 +24,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.cloningtokopedia.R
-import com.example.cloningtokopedia.modifier.CustomModifier.Companion.customSearchField
 import com.example.cloningtokopedia.ui.theme.CloningTokopediaTheme
 import com.example.cloningtokopedia.ui.theme.accentGray200
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CustomSeacrhBar(
+fun CustomSearch(
     query: String,
     icon: Painter,
     iconSize: Dp = 24.dp,
@@ -103,7 +98,7 @@ fun CustomSeacrhBar(
 @Composable
 fun SearchFieldPreview() {
     CloningTokopediaTheme {
-        CustomSeacrhBar(
+        CustomSearch(
             query = "",
             icon = painterResource(id = R.drawable.icn_search),
             iconSize = 12.dp,
@@ -117,7 +112,7 @@ fun SearchFieldPreview() {
                     shape = RoundedCornerShape(7.dp)
                 ),
             placeholder = {
-                CustomTextView(
+                CustomText(
                     text = "Cari di Tokopedia",
                     fontSize = 12.toDouble(),
                     color = accentGray200
