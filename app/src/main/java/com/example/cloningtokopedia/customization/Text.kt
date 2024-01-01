3f4c5e8485bc6f2a98f816fac436c63e31a8c078
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2024-01-01 19:33:25 +0700

    > renamed: CustomTextView.kt -> Text.kt > renamed: CustomTextView -> CustomText (composable function) > add annotatedString parameter > add null check for annotatedString (if !null -> return composable text with annotatedString else return composable text without annotatedString)

--- app/src/main/java/com/example/cloningtokopedia/customization/Text.kt
+++ app/src/main/java/com/example/cloningtokopedia/customization/Text.kt
@@ -1,6 +1,6 @@
 package com.example.cloningtokopedia.customization
 
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
@@ -9,13 +9,23 @@ import androidx.compose.ui.unit.sp
 import com.example.cloningtokopedia.ui.theme.customFont
 
 @Composable
-fun CustomAddressTextView(
-    text: AnnotatedString,
+fun CustomText(
+    text: String = "",
+    annotatedText: AnnotatedString? = null,
     fontSize: Double,
-    color: Color = Color.Black,
     fontWeight: String = "regular",
+    color: Color = Color.Black
 ) {
-    Text(
+    annotatedText?.let {
+        Text(
+            text = it,
+            style = TextStyle(
+                color = color,
+                fontSize = fontSize.sp,
+                fontFamily = customFont(weight = fontWeight)
+            )
+        )
+    } ?: Text(
         text = text,
         style = TextStyle(
             color = color,
