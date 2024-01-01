7d78794cdbcf355637416d8c872fb249e36a410d
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2024-01-01 18:40:12 +0700

    update colors name

--- app/src/main/java/com/example/cloningtokopedia/ui/theme/Color.kt
+++ app/src/main/java/com/example/cloningtokopedia/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.example.cloningtokopedia.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// default colors
 val Pink40 = Color(0xFF7D5260)
 val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
@@ -10,20 +9,15 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey40 = Color(0xFF625b71)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 
-// primary and secondary colors
-val primaryColor = Color(0xFF00AA5B)
-val secondaryColor = Color(0xFFFFFFFF)
-val accentColor = Color(0xFF6F7585)
+val primaryColor = Color(0xFFFFFFFF)
 
-// colors for text or colors accent
-val primaryTextColor = Color(0xFF000000)
-val secondaryTextColor = Color(0xFF6F7585)
-val accentTextColor = Color(0xFFFFFFFF)
-val accentErrorTextColor = Color(0xFFE02954)
-val accentSuccessTextColor = Color(0xFF00AA5B)
+val accentGray100 = Color(0xFFD7DFEA)
+val accentGray200 = Color(0xFF6F7585)
+val accentGray300 = Color(0xFF525A67)
 
-// other colors
-val badgeColor = Color(0xFFE02954)
-val errorBackgroundTextColor = Color(0xFFFFEAEF)
-val successBackgroundTextColor = Color(0xFFD6FFDD)
-val borderColor = Color(0xFFD7DFEA)
+val accentRed = Color(0xFFE02954)
+val accentGreen = Color(0xFF00AA5B)
+val accentYellow = Color(0xFFFBC117)
+val accentPurple = Color(0xFF7F37D7)
+val accentRed100 = Color(0xFFFFEAEF)
+val accentGreen100 = Color(0xFFD6FFDD)
