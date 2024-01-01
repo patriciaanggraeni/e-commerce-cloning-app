5189e8a9771d18a161127879a3c79ba6e8141bcf
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2024-01-01 19:46:58 +0700

    > remove unused import > change composable function call with CustomSearch > update details

--- app/src/main/java/com/example/cloningtokopedia/composable/AppBar.kt
+++ app/src/main/java/com/example/cloningtokopedia/composable/AppBar.kt
@@ -1,10 +1,14 @@
 package com.example.cloningtokopedia.composable
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,11 +18,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cloningtokopedia.R
 import com.example.cloningtokopedia.customization.CustomIcon
-import com.example.cloningtokopedia.customization.CustomSearchField
+import com.example.cloningtokopedia.customization.CustomSearch
+import com.example.cloningtokopedia.customization.CustomText
 import com.example.cloningtokopedia.modifier.CustomModifier.Companion.customSearchField
 import com.example.cloningtokopedia.ui.theme.CloningTokopediaTheme
-import com.example.cloningtokopedia.ui.theme.accentColor
-import com.example.cloningtokopedia.ui.theme.secondaryTextColor
+import com.example.cloningtokopedia.ui.theme.accentGray200
 
 @Composable
 fun CustomAppBar(modifier: Modifier) {
@@ -33,28 +37,33 @@ fun CustomAppBar(modifier: Modifier) {
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        CustomSearchField(
-            modifier = Modifier
-                .customSearchField(
-                    width = 200.dp,
-                    height = 35.dp,
-                    cornerSize = 7.dp
-                )
-                .padding(10.dp),
-            spacer = 10.dp,
-            placeholder = "Cari di Tokopedia",
-            placeholderTextSize = 12,
-            placeholderColor = secondaryTextColor,
-            icon = painterResource(id = R.drawable.icn_search),
-            iconSize = 10.dp,
-            iconTint = accentColor,
+       CustomSearch(
+           query = "",
+           icon = painterResource(id = R.drawable.icn_search),
+           iconSize = 12.dp,
+           iconTint = accentGray200,
+           modifier = Modifier
+               .width(200.dp)
+               .height(35.dp)
+               .border(
+                   width = 1.dp,
+                   color = accentGray200,
+                   shape = RoundedCornerShape(7.dp)
+               ),
+           placeholder = {
+               CustomText(
+                   text = "Cari di Tokopedia",
+                   fontSize = 12.toDouble(),
+                   color = accentGray200
+               )
+           }
         )
 
         listIcons.forEach {
             CustomIcon(
                 icon = it,
                 size = 20.dp,
-                tint = accentColor
+                tint = accentGray200
             )
         }
     }
